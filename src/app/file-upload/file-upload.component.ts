d9ab2a527cbed0fb4dcabf4e6f5c5aab7703a305
Sai Kishore <saikishore.kodimela@lji.io>
2023-09-06 16:10:42 +0530

    file upload done

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./file-upload.component.less'],
 })
 export class FileUploadComponent {
-  uploadFile(eventData: any) {}
+  constructor(private http: HttpClient, private router: ActivatedRoute) {}
+  errorMessage: any = '';
+
+  uploadFile(fileData: any) {
+    let requestUrl: any = JSON.parse(localStorage.getItem('scanner_url')!);
+    let member_id = this.router.snapshot.params['member_id'];
+    requestUrl = requestUrl.url + '/' + member_id;
+    
+    let formData = new FormData();
+    formData.append('file', fileData.target.files[0]);
+
+    this.http.post(requestUrl, formData).subscribe(
+      (response: any) => {
+        this.errorMessage = 'Success';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 3000);
+      },
+      (respose: any) => {
+        console.log(respose);
+        this.errorMessage = respose.error.message;
+      }
+    );
+  }
 }
